0ed827340b7271433ff23913dadc0760bc50d02e
Thomas Westerhoff <thomas.westerhoff24@gmx.de>
2016-07-04 10:59:56 +0200

    5

--- TR603mc/module.php
+++ TR603mc/module.php
@@ -1,15 +1,8 @@
 <?
-
+//Klassendefinition
 class STECA extends IPSModule
 {
 
-    //--------------------------------------------------------
-    // main module functions
-    //--------------------------------------------------------
-    /**
-     * Constructor.
-     * @param $InstanceID
-     */
     public function __construct($InstanceID)
     {
         // Diese Zeile nicht löschen
@@ -17,32 +10,18 @@ class STECA extends IPSModule
 
     }
 
-    //--------------------------------------------------------
-    /**
-     * overload internal IPS_Create($id) function
-     */
     public function Create()
     {
         // Diese Zeile nicht löschen.
         parent::Create();
-        //Hint: $this->debug will not work in this stage! must use IPS_LogMessage
-        //props
-        $this->RegisterPropertyString('Category', 'STECA Devices');
-        $this->RegisterPropertyInteger('ParentCategory', 0); //parent cat is root
-
-        $this->RegisterPropertyString('LogFile', '');
-        //$this->RegisterPropertyInteger('RainPerCount', 295);
-        $this->RegisterPropertyBoolean('AutoCreate', true);
-        $this->RegisterPropertyBoolean('Debug', false);
-        $this->RegisterPropertyBoolean('Active', false);
-
-        //Vars
-        $this->RegisterVariableString('Buffer', 'Buffer', "", -1);
-        IPS_SetHidden($this->GetIDForIdent('Buffer'), true);
-        $this->RegisterVariableString('LastUpdate', 'Last Update', "", -4);
-        IPS_SetHidden($this->GetIDForIdent('LastUpdate'), true);
-     
-    }
+	}
+	
+	// Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+        }
+		
     //--------------------------------------------------------
     /**
      * Destructor
@@ -53,3 +32,4 @@ class STECA extends IPSModule
     }
     //function
 }//class
+?>
