13c973be8d97ee6b1ce4fdf6c2b3e653f0ebb0a7
Thomas Westerhoff <thomas.westerhoff24@gmx.de>
2016-07-04 11:34:14 +0200

    5

--- TR603mc/module.php
+++ TR603mc/module.php
@@ -27,11 +27,28 @@ class STECA extends IPSModule
         $this->RegisterPropertyBoolean('Debug', false);
         $this->RegisterPropertyBoolean('Active', false);
 
+		//Profile
+	//	$this->RegisterVariableProfile('TempSolar', 'Temperatur','', ' °C', -200, 500, 1);
+    //    $this->RegisterVariableProfile('PowSolar','','',' kW',0,1000,0.1,1);
+    //    $this->RegisterVariableProfile('FlowSolar','','',' l/min',0,50,1);
+    //    $this->RegisterVariableProfile('AlarmSolar','','ALARM','',$FF0000,'Kein','',$00FF00);
+    //    $this->RegisterVariableProfile('WMZSolar','','An','',$00FF00,'Aus','',$FF0000);
+
+		
         //Vars
         $this->RegisterVariableString('Buffer', 'Buffer', "", -1);
         IPS_SetHidden($this->GetIDForIdent('Buffer'), true);
         $this->RegisterVariableString('LastUpdate', 'Last Update', "", -4);
         IPS_SetHidden($this->GetIDForIdent('LastUpdate'), true);
+        $this->RegisterVariableInteger('T1', 'T1', "");
+        $this->RegisterVariableInteger('T2', 'T2', "");
+        $this->RegisterVariableInteger('T3', 'T3', "");
+        $this->RegisterVariableInteger('T4', 'T4', "");
+        $this->RegisterVariableInteger('T5', 'T5', "");
+        $this->RegisterVariableInteger('T6', 'T6', "");
+        $this->RegisterVariableInteger('R1', 'R1', "");
+        $this->RegisterVariableInteger('R2', 'R2', "");
+        $this->RegisterVariableInteger('R3', 'R3', "");
 
 
         //Connect Parent
