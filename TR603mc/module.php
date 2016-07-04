a6640bfbcb4fbf9978b93b8536f4379f23293dbe
Thomas Westerhoff <thomas.westerhoff24@gmx.de>
2016-07-04 10:47:45 +0200

    4

--- TR603mc/module.php
+++ TR603mc/module.php
@@ -1,44 +1,7 @@
 <?
-/**
- * @file
- *
- * STECA Gateway IPSymcon PHP Splitter Module Class
- *
- * @author Thomas Westerhoff
- * @copyright Thomas Westerhoff 2009-2016
- * @version 1.0
- * @date 2016-05-04
- */
 
-
-//include_once(__DIR__ . "/../module_helper.php");
-
-/** @class STECA
- *
- * STECA Gateway IPSymcon PHP Splitter Module Class
- *
- *
- */
 class STECA extends IPSModule
 {
-    //------------------------------------------------------------------------------
-    //module const and vars
-    //------------------------------------------------------------------------------
-    /**
-     * Timer constant
-     * maxage of LastUpdate in sec before ReInit
-     */
-    const MAXAGE = 300;
-    /**
-     * How many sensors are attached
-     * 8 external Temp/Hum Sensors(0-7), Kombisensor(8) and indoor Sensors(9)
-     */
-    const MAXSENSORS = 1; //no indoor record
-
-    /**
-     * Fieldlist for Logging
-     */
-    const fieldlist = "Time;Typ;Id;Name;Temp;Hum;Bat;Lost;Wind;Rain;IsRaining;RainCounter;Pressure;willi;";
 
     //--------------------------------------------------------
     // main module functions
@@ -50,8 +13,7 @@ class STECA extends IPSModule
     public function __construct($InstanceID)
     {
         // Diese Zeile nicht löschen
-        $json = __DIR__ . "/module.json";
-        parent::__construct($InstanceID, $json);
+        parent::__construct($InstanceID);
 
     }
 
@@ -79,30 +41,7 @@ class STECA extends IPSModule
         IPS_SetHidden($this->GetIDForIdent('Buffer'), true);
         $this->RegisterVariableString('LastUpdate', 'Last Update', "", -4);
         IPS_SetHidden($this->GetIDForIdent('LastUpdate'), true);
-
-
-        //Timers
-        $this->RegisterTimer('ReInit', 60000, $this->module_data["prefix"] . '_ReInitEvent($_IPS[\'TARGET\']);');
-
-
-        //Connect Parent
-        $this->RequireParent($this->module_interfaces['SerialPort']);
-        $pid = $this->GetParent();
-        if ($pid) {
-            $name = IPS_GetName($pid);
-            if ($name == "Serial Port") IPS_SetName($pid, __CLASS__ . " Port");
-        }
-
-        //call init if ready and activated
-        if (IPS_GetKernelRunlevel() == self::KR_READY) {
-            if ($this->isActive()) {
-                $this->SetStatus(self::ST_AKTIV);
-                $this->init();
-            } else {
-                $this->SetStatus(self::ST_INACTIV);
-                $this->SetTimerInterval('ReInit', 0);
-            }
-        }
+     
     }
     //--------------------------------------------------------
     /**
