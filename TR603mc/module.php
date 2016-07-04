ba49e5c9ba9cf4ef8b9e85ed7f2aec079b84577b
Thomas Westerhoff <thomas.westerhoff24@gmx.de>
2016-07-04 11:13:15 +0200

    6

--- TR603mc/module.php
+++ TR603mc/module.php
@@ -1,4 +1,9 @@
 <?
+
+//Steca Solarregler Modul
+// Thomas Westerhoff
+
+
 //Klassendefinition
 class STECA extends IPSModule
 {
@@ -14,6 +19,40 @@ class STECA extends IPSModule
     {
         // Diese Zeile nicht löschen.
         parent::Create();
+		
+        $this->RegisterPropertyString('Category', 'STECA Devices');
+        $this->RegisterPropertyInteger('ParentCategory', 0); //parent cat is root
+        $this->RegisterPropertyString('LogFile', '');
+        $this->RegisterPropertyBoolean('AutoCreate', true);
+        $this->RegisterPropertyBoolean('Debug', false);
+        $this->RegisterPropertyBoolean('Active', false);
+
+        //Vars
+        $this->RegisterVariableString('Buffer', 'Buffer', "", -1);
+        IPS_SetHidden($this->GetIDForIdent('Buffer'), true);
+        $this->RegisterVariableString('LastUpdate', 'Last Update', "", -4);
+        IPS_SetHidden($this->GetIDForIdent('LastUpdate'), true);
+
+
+        //Connect Parent
+        $this->RequireParent($this->module_interfaces['SerialPort']);
+        $pid = $this->GetParent();
+        if ($pid) {
+            $name = IPS_GetName($pid);
+            if ($name == "Serial Port") IPS_SetName($pid, __CLASS__ . " Port");
+        }
+
+        //call init if ready and activated
+        if (IPS_GetKernelRunlevel() == self::KR_READY) {
+            if ($this->isActive()) {
+                $this->SetStatus(self::ST_AKTIV);
+                $this->init();
+            } else {
+                $this->SetStatus(self::ST_INACTIV);
+                $this->SetTimerInterval('ReInit', 0);
+            }
+        }
+		
 	}
 	
 	// Überschreibt die intere IPS_ApplyChanges($id) Funktion
